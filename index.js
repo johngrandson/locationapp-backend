8492ef8d3407b0befa85d2c860f14430db5bab80
João Netto <joaonettopb@hotmail.com>
2018-09-24 21:48:05 -0300

    atualizacao axios

--- index.js
+++ index.js
@@ -10,32 +10,45 @@ const app = express()
 const server = http.createServer(app)
 const io = socketIo(server)
 
-const locationMap = new Map()
+const axios = require('axios');
+const url = 'http://api.olhovivo.sptrans.com.br/v2.1'
+
+const OlhoVivo = require('olhovivo')
+
+const token = 'f178380a3dd64fc801f25467748f4bbbeb8f37e501a7172318c637d75ed6d112'
+
+const olhoVivo = new OlhoVivo({
+  token: token
+})
+
+olhoVivo.authenticate('f178380a3dd64fc801f25467748f4bbbeb8f37e501a7172318c637d75ed6d112')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.send('Hello!')
-})
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-io.on('connection', socket => {
-  socket.on('updateLocation', pos => {
-    locationMap.set(socket.id, pos)
-  })
-
-  socket.on('requestLocations', () => {
-    socket.emit('locationsUpdate', Array.from(locationMap))
-  })
-
-  socket.on('disconnect', () => {
-    locationMap.delete(socket.id)
-  })
+app.get('/api/:id', (req, res, next) => {
+  module.exports = olhoVivo.linePositions('1100')
+    .then(res => {
+      const busLoc = {
+        'Hora': res.hr,
+        'Lat': res.vs[0].py,
+        'Lng': res.vs[0].px
+      }
+      console.log(busLoc);
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 server.listen(3000, err => {
   if (err) {
     throw err
   }
-
   console.log('server started on port 3000')
 })
--- public/js/tracker.js
+++ public/js/tracker.js
@@ -1,18 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const socket = io('/')
-
-  const positionOptions = {
-    enableHighAccuracy: true,
-    maximumAge: 0
-  }
-
-  setInterval(() => {
-    navigator.geolocation.getCurrentPosition(pos => {
-      const { latitude: lat, longitude: lng } = pos.coords
-      socket.emit('updateLocation', { lat, lng })
-    }, err => {
-      console.error(err)
-    }, positionOptions)
-  }, 2000)
-
-})
+function getBus() {
+    alert('ola')
+    axios.get('localhost:3000/api')
+        .then((response) => {
+            alert(response)
+        })
+        .catch((error) => {
+            console.log(error);;
+        });
+}
